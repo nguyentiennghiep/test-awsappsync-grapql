dfbdf6e28a38afbeb640ff78ed3b17451f4b7d1d
nguyen tien nghiep <nguyen_tien_nghiep@mediado.local>
2018-07-05 15:14:06 +0900

    first commit

---
+++ src/AppSync.js
@@ -0,0 +1,6 @@
+﻿export default {
+    "graphqlEndpoint": "https://uwfqwycpnvepphznjbgtish5qe.appsync-api.us-east-2.amazonaws.com/graphql",
+    "region": "us-east-2",
+    "authenticationType": "API_KEY",
+    "apiKey": "da2-7433pmitpvethdwqgfu3w7mgui"
+}
